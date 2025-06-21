2410e8a594dcdbc50c31b45a68c871572e00c8ee
SolidPrincess <glden.chi@gmail.com>
2025-06-21 14:28:28 +0500

    del

--- src/MinerGame.java
+++
@@ -1,8 +0,0 @@
-public class MinerGame {
-
-	public static void main(String[] args) {
-		FieldEngineHelper engine = new FieldEngineHelper();
-		GameGUIEngine view = new GameGUIEngine(engine);
-	}
-
-}

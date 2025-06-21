b803132b753bf810468724dcde6895bd0377b5c9
SolidPrincess <glden.chi@gmail.com>
2025-06-21 14:31:36 +0500

    add src folder

---
+++ src/MinerGame.java
@@ -0,0 +1,8 @@
+public class MinerGame {
+
+	public static void main(String[] args) {
+		FieldEngineHelper engine = new FieldEngineHelper();
+		GameGUIEngine view = new GameGUIEngine(engine);
+	}
+
+}
